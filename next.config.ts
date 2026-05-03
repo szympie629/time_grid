5282ee07181a09094c78782381331192f29e39a6
szympie629 <szymon.piela.98@gmail.com>
2026-05-03 11:09:36 +0200

    chore: add required packages to transpilePackages in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  transpilePackages: ['lucide-react', '@dnd-kit/core', '@dnd-kit/sortable', '@dnd-kit/utilities', 'react-resizable-panels'],
 };
 
 export default nextConfig;
